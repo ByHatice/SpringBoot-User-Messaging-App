a8997b011fad3e2d438db2be56c67c65cccd030c
Hatice Celik <hatice.celik@iths.se>
2024-03-20 16:07:21 +0100

    Initial commit

---
+++ src/test/java/se/iths/springbootusermessagingapp/SpringBootUserMessagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.springbootusermessagingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUserMessagingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
